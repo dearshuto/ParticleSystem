e7207b3200ead03920f7daa5116478117dec84d0
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-02-19 16:32:29 +0900

    [add] Add the main class ParticleSystem

--- include/ParticleSystem/particle_system.hpp
+++ include/ParticleSystem/particle_system.hpp
@@ -13,14 +13,15 @@
 #include <string>
 #include <vector>
 
-#include "collision_simulator.hpp"
-#include "particle.hpp"
+#include <ParticleSystem/type/Scalar.h>
+#include <ParticleSystem/type/Vector.hpp>
 
-namespace mlk {
+namespace fj {
+    class Particle;
     class ParticleSystem;
 }
 
-class mlk::ParticleSystem
+class fj::ParticleSystem
 {
 public:
     ParticleSystem() = default;
@@ -28,53 +29,33 @@ public:
     
     void stepSimulation(const float timestep);
     
-    void computeInteraction(std::unique_ptr<mlk::CollisionObjectPair> collisionPair)const;
-    
-    void createParticle();
     
     void createParticleAt(const float x, const float y, const float z);
     
-    /**
-     * Load an OBJ model from file system
-     */
-    bool loadModelFromFile(const std::string& filename);
-    
 private:
-    void applyForceUsingSimulator(const float timestep);
     void applyGravity();
     
 //ge tters & setters
 public:
-    const std::vector<std::shared_ptr<mlk::Particle>>& getParticles()const
+    const std::vector<std::shared_ptr<fj::Particle>>& getParticles()const
     {
         return m_particles;
     }
     
-    void setCollisionSimulator(std::unique_ptr<mlk::CollisionSimulator> simulator)
-    {
-        m_simulator = std::move(simulator);
-    }
-    
-    const mlk::Vector3& getGravity()const
+    const fj::Vector& getGravity()const
     {
         return m_gravity;
     }
     
     void setGravity(const float x, const float y, const float z)
     {
-        m_gravity = mlk::Vector3(x, y, z);
+        m_gravity = fj::Vector(x, y, z);
     }
     
-protected:
-    mlk::CollisionSimulator* getCollisionSimulatorPtr()
-    {
-        return m_simulator.get();
-    }
     
 private:
-    std::vector<std::shared_ptr<mlk::Particle>> m_particles;
-    std::unique_ptr< mlk::CollisionSimulator > m_simulator;
-    mlk::Vector3 m_gravity;
+    std::vector< std::shared_ptr<fj::Particle> > m_particles;
+    fj::Vector m_gravity;
 };
 
 #endif /* particle_system_hpp */
