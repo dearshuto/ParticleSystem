2a22a618c92f1c608ae9bff157437fe9e5935247
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-03-18 19:30:35 +0900

    [add] Add the new class for neighbor search

---
+++ include/ParticleSystem/neighbor_search/NeighborMap2D.hpp
@@ -0,0 +1,21 @@
+//
+//  NeighborMap2D.hpp
+//  ParticleSystem
+//
+//  Created by Shuto Shikama on 2016/03/18.
+//
+//
+
+#ifndef NeighborMap2D_hpp
+#define NeighborMap2D_hpp
+
+namespace fj {
+    class NeighborMap2D;
+}
+
+class fj::NeighborMap2D{
+    NeighborMap2D() = default;
+    ~NeighborMap2D() = default;
+};
+
+#endif /* NeighborMap2D_hpp */
---
+++ src/neighbor_search/NeighborMap2D.cpp
@@ -0,0 +1,9 @@
+//
+//  NeighborMap2D.cpp
+//  ParticleSystem
+//
+//  Created by Shuto Shikama on 2016/03/18.
+//
+//
+
+#include <ParticleSystem/neighbor_search/NeighborMap2D.hpp>
