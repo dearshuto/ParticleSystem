866917ee500c79d7d27a0a829d336d7957ed3054
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-03-23 18:18:10 +0900

    [add] Add the new class to manage particles

---
+++ include/ParticleSystem/neighbor_search/particle_map.hpp
@@ -0,0 +1,30 @@
+//
+//  particle_map.hpp
+//  ParticleSystem
+//
+//  Created by Shuto Shikama on 2016/03/23.
+//
+//
+
+#ifndef particle_map_hpp
+#define particle_map_hpp
+
+#include <memory>
+
+namespace fj {
+    class Particle;
+    class ParticleMap;
+}
+
+class fj::ParticleMap
+{
+public:
+    ParticleMap() = delete;
+    ~ParticleMap() = default;
+    
+    virtual void registerParticle(std::shared_ptr<fj::Particle> particle) = 0;
+    
+    virtual const fj::Particle& getParticleAt(const unsigned int id)const = 0;
+};
+
+#endif /* particle_map_hpp */
