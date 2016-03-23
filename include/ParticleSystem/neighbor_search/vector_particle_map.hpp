9607b71b2926a71873620ab893f3e076655f965b
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-03-23 18:25:38 +0900

    [add] Add the new class to manage particles using std::vector

---
+++ include/ParticleSystem/neighbor_search/vector_particle_map.hpp
@@ -0,0 +1,14 @@
+//
+//  vector_particle_map.hpp
+//  ParticleSystem
+//
+//  Created by Shuto Shikama on 2016/03/23.
+//
+//
+
+#ifndef vector_particle_map_hpp
+#define vector_particle_map_hpp
+
+#include <stdio.h>
+
+#endif /* vector_particle_map_hpp */
---
+++ src/neighbor_search/vector_particle_map.cpp
@@ -0,0 +1,9 @@
+//
+//  vector_particle_map.cpp
+//  ParticleSystem
+//
+//  Created by Shuto Shikama on 2016/03/23.
+//
+//
+
+#include "vector_particle_map.hpp"
