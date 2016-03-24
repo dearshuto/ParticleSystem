6a641302e50eeaaec5b6401c616734387aa00873
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-03-24 16:05:50 +0900

    [add] Add the new system

---
+++ include/ParticleSystem/particle_manager/particle_manager.hpp
@@ -0,0 +1,14 @@
+//
+//  particle_manager.hpp
+//  ParticleSystem
+//
+//  Created by Shuto Shikama on 2016/03/24.
+//
+//
+
+#ifndef particle_manager_hpp
+#define particle_manager_hpp
+
+#include <stdio.h>
+
+#endif /* particle_manager_hpp */
---
+++ src/particle_manager/particle_manager.cpp
@@ -0,0 +1,9 @@
+//
+//  particle_manager.cpp
+//  ParticleSystem
+//
+//  Created by Shuto Shikama on 2016/03/24.
+//
+//
+
+#include "particle_manager.hpp"
