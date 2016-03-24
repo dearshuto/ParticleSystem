3bf018b84dadffec2ef9f4bedc74d0fc13024f4e
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-03-24 23:08:04 +0900

    [add] Add the function to compute a hash value

--- include/ParticleSystem/collision_dispatcher/particle_collision_dispatcher.hpp
+++ include/ParticleSystem/collision_dispatcher/particle_collision_dispatcher.hpp
@@ -9,6 +9,55 @@
 #ifndef particle_collision_dispatcher_hpp
 #define particle_collision_dispatcher_hpp
 
-#include <stdio.h>
+#include <memory>
+#include <vector>
+
+namespace fj {
+    class Particle;
+    class ParticleCollisionDispatcher;
+}
+
+class fj::ParticleCollisionDispatcher
+{
+    typedef unsigned int HashValue;
+public:
+    ParticleCollisionDispatcher() = delete;
+    ~ParticleCollisionDispatcher() = default;
+    
+    ParticleCollisionDispatcher(const unsigned int width, const unsigned int height, const unsigned int depth)
+    : m_width(width)
+    , m_height(height)
+    , m_depth(depth)
+    {
+        
+    }
+    
+    std::vector<const std::shared_ptr<fj::Particle>> getNeighborParticlesAt(const fj::Particle& particle)const;
+    
+private:
+    
+    HashValue computeHash(const fj::Particle& particle)const;
+    
+private:
+    const unsigned int getWidth()const
+    {
+        return m_width;
+    }
+    
+    const unsigned int getHeight()const
+    {
+        return m_height;
+    }
+    
+    const unsigned int getDepth()const
+    {
+        return m_depth;
+    }
+    
+private:
+    const unsigned int m_width;
+    const unsigned int m_height;
+    const unsigned int m_depth;
+};
 
 #endif /* particle_collision_dispatcher_hpp */
--- src/collision_dispatcher/particle_collision_dispatcher.cpp
+++ src/collision_dispatcher/particle_collision_dispatcher.cpp
@@ -6,4 +6,25 @@
 //
 //
 
+#include <FUJIMath/type/scalar.h>
+#include <ParticleSystem/particle/particle.hpp>
+
 #include <ParticleSystem/collision_dispatcher/particle_collision_dispatcher.hpp>
+
+std::vector<const std::shared_ptr<fj::Particle>> fj::ParticleCollisionDispatcher::getNeighborParticlesAt(const fj::Particle &particle)const
+{
+    std::vector<const std::shared_ptr<fj::Particle>> neighborParticles;
+    
+    
+    return neighborParticles;
+}
+
+fj::ParticleCollisionDispatcher::HashValue fj::ParticleCollisionDispatcher::computeHash(const fj::Particle& particle)const
+{
+    const fj::Vector3& kPosition = particle.getPosition();
+    const fj::Scalar& kX = kPosition.x();
+    const fj::Scalar& kY = kPosition.y();
+    const fj::Scalar& kZ = kPosition.z();
+    
+    return kX + getWidth() * kY + getWidth() * getHeight() * kZ;
+}
