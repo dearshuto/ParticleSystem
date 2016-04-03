864c56fe94072af1faee6993cbf0b493e26762d4
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-04-03 16:40:19 +0900

    [add] Add the new class to manage neighbor relations

---
+++ include/ParticleSystem/particle_manager/neighbor_map.hpp
@@ -0,0 +1,23 @@
+//
+//  neighbor_map.hpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/04/03.
+//
+//
+
+#ifndef neighbor_map_hpp
+#define neighbor_map_hpp
+
+namespace fj {
+    class NeighborMap;
+}
+
+class fj::NeighborMap
+{
+public:
+    NeighborMap() = default;
+    ~NeighborMap() = default;
+};
+
+#endif /* neighbor_map_hpp */
---
+++ src/particle_manager/neighbor_map.cpp
@@ -0,0 +1,9 @@
+//
+//  neighbor_map.cpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/04/03.
+//
+//
+
+#include <ParticleSystem/particle_manager/neighbor_map.hpp>
