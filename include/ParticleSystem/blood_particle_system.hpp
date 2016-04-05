f722e0f2c3668da869c17f476a80d3e69bddc5d6
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-04-05 16:11:59 +0900

    [add] Make it support the blood system

---
+++ include/ParticleSystem/blood_particle_system.hpp
@@ -0,0 +1,35 @@
+//
+//  blood_particle_system.hpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/04/05.
+//
+//
+
+#ifndef blood_particle_system_hpp
+#define blood_particle_system_hpp
+
+#include <memory>
+#include "particle_system.hpp"
+
+namespace fj {
+    class BloodSPHMethod;
+    class CollisionDispatcher;
+    class Solver;
+    class BloodParticleSystem;
+}
+
+class fj::BloodParticleSystem : public fj::ParticleSystem
+{
+public:
+    BloodParticleSystem() = delete;
+    ~BloodParticleSystem() = default;
+    
+    BloodParticleSystem(std::unique_ptr<fj::BloodSPHMethod> solver, std::unique_ptr<fj::ParticleCollisionDispatcher> collisionDispatcher = nullptr)
+    : ParticleSystem(std;;move(solver), std::move(collisionDispatcher))
+    {
+        
+    }
+};
+
+#endif /* blood_particle_system_hpp */
---
+++ src/blood_particle_system.cpp
@@ -0,0 +1,9 @@
+//
+//  blood_particle_system.cpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/04/05.
+//
+//
+
+#include <ParticleSystem/particle_system.hpp>
