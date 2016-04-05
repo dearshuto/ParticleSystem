6d342532e09d23b8aad97d91dc3e8aba9104057e
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-04-05 16:01:38 +0900

    [add] Add the new SPH solver

---
+++ include/ParticleSystem/solver/BloodSPHMethod.hpp
@@ -0,0 +1,31 @@
+//
+//  BloodSPHMethod.hpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/04/05.
+//
+//
+
+#ifndef BloodSPHMethod_hpp
+#define BloodSPHMethod_hpp
+
+#include "sph_method.hpp"
+
+namespace fj {
+    class BloodSPHMethod;
+}
+
+class fj::BloodSPHMethod : public SPHMethod
+{
+public:
+    BloodSPHMethod() = default;
+    ~BloodSPHMethod() = default;
+    
+    const fj::Scalar getViscosity()const override
+    {
+        return 1.0;
+    }
+
+};
+
+#endif /* BloodSPHMethod_hpp */
---
+++ src/solver/BloodSPHMethod.cpp
@@ -0,0 +1,9 @@
+//
+//  BloodSPHMethod.cpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/04/05.
+//
+//
+
+#include <ParticleSystem/solver/BloodSPHMethod.hpp>
