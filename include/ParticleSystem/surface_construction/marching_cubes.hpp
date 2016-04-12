76eea884be6c4c5f85c59aa6945f35415fa28192
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-04-12 17:05:11 +0900

    [add] Add surface construction algorithm

---
+++ include/ParticleSystem/surface_construction/marching_cubes.hpp
@@ -0,0 +1,14 @@
+//
+//  marching_cubes.hpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/04/12.
+//
+//
+
+#ifndef marching_cubes_hpp
+#define marching_cubes_hpp
+
+#include <stdio.h>
+
+#endif /* marching_cubes_hpp */
---
+++ src/surface_construction/marching_cubes.cpp
@@ -0,0 +1,9 @@
+//
+//  marching_cubes.cpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/04/12.
+//
+//
+
+#include <ParticleSystem/surface_construction/marching_cubes.hpp>
