2ee061defc211f4d1affb0b84db3f4e3b56a6c37
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-04-12 19:41:31 +0900

    [add] Make it support simulation data

--- include/ParticleSystem/surface_construction/marching_cubes.hpp
+++ include/ParticleSystem/surface_construction/marching_cubes.hpp
@@ -9,6 +9,8 @@
 #ifndef marching_cubes_hpp
 #define marching_cubes_hpp
 
+#include <tuple>
+
 namespace fj {
     class MarchingCubes;
 }
@@ -16,8 +18,45 @@ namespace fj {
 class fj::MarchingCubes
 {
 public:
+    typedef std::pair<int , int> Range_t;
+public:
     MarchingCubes() = default;
-    ~MarchingCubes() = default;
+    virtual~MarchingCubes() = default;
+    
+    MarchingCubes(const Range_t& xRange, const Range_t& yRange, const Range_t& zRange, const float resolution)
+    : m_xRange(xRange)
+    , m_yRange(yRange)
+    , m_zRange(zRange)
+    , m_resolution(resolution)
+    {
+        
+    }
+    
+    const Range_t& getRangeX()const
+    {
+        return m_xRange;
+    }
+
+    const Range_t& getRangeY()const
+    {
+        return m_yRange;
+    }
+
+    const Range_t& getRangeZ()const
+    {
+        return m_zRange;
+    }
+
+    float getResolution()const
+    {
+        return m_resolution;
+    }
+    
+private:
+    const Range_t m_xRange;
+    const Range_t m_yRange;
+    const Range_t m_zRange;
+    const float m_resolution;
 };
 
 #endif /* marching_cubes_hpp */
