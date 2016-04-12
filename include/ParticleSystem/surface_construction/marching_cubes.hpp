0e40c6e72be25021b69c2f93cee5ac609eee0aa1
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-04-12 19:52:45 +0900

    [add]

--- include/ParticleSystem/surface_construction/marching_cubes.hpp
+++ include/ParticleSystem/surface_construction/marching_cubes.hpp
@@ -10,6 +10,9 @@
 #define marching_cubes_hpp
 
 #include <tuple>
+#include <vector>
+
+#include <FUJIMath/type/vector3.hpp>
 
 namespace fj {
     class MarchingCubes;
@@ -17,6 +20,7 @@ namespace fj {
 
 class fj::MarchingCubes
 {
+    typedef std::tuple<unsigned int, unsigned int, unsigned int> TriangleIndex_t;
 public:
     typedef std::pair<int , int> Range_t;
 public:
@@ -37,6 +41,7 @@ public:
      */
     void execute();
     
+public:
     const Range_t& getRangeX()const
     {
         return m_xRange;
@@ -57,11 +62,36 @@ public:
         return m_resolution;
     }
     
+    const std::vector<fj::Vector3>& getVertices()const
+    {
+        return m_vertices;
+    }
+    
+    const std::vector<TriangleIndex_t>& getTriangleIndices()const
+    {
+        return m_triangleIndices;
+    }
+
+private:
+    std::vector<fj::Vector3>* getVertices()
+    {
+        return &m_vertices;
+    }
+    
+    std::vector<TriangleIndex_t>* getTriangleIndices()
+    {
+        return &m_triangleIndices;
+    }
+
+    
 private:
     const Range_t m_xRange;
     const Range_t m_yRange;
     const Range_t m_zRange;
     const float m_resolution;
+    
+    std::vector<fj::Vector3> m_vertices;
+    std::vector<TriangleIndex_t> m_triangleIndices;
 };
 
 #endif /* marching_cubes_hpp */
