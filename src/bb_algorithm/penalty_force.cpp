7f212f684b931e8c1240ae982ecff2f294b23999
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-04-23 18:24:15 +0900

    [add] Make it support penalty force

--- src/bb_algorithm/penalty_force.cpp
+++ src/bb_algorithm/penalty_force.cpp
@@ -15,49 +15,50 @@
 #include <ParticleSystem/particle_manager/particle_manager.hpp>
 #include <ParticleSystem/bb_algorithm/penalty_force.hpp>
 
-void fj::PenaltyForce::execute(fj::ParticleSystem* particleSystem)
+void fj::PenaltyForce::executeBBAlgorithm(fj::ParticleSystem* particleSystem)
 {
-    m_bb->execute(particleSystem);
-    
     const fj::BoundingBox::Range& kRangeX = getBoundingBox().getRangeX();
     const fj::BoundingBox::Range& kRangeY = getBoundingBox().getRangeY();
     const fj::BoundingBox::Range& kRangeZ = getBoundingBox().getRangeZ();
-    
 
-//    for (auto& particle : particleManager)
-//    {
-//        const fj::Vector3& kPosition = particle->getPosition();
-//        
-//        if( fj::Scalar kIX = (kRangeX.getMin() - kPosition.x()) > 0)
-//        {
-//            particle->addVelocity( m_K * kIX * fj::Vector3(0.1, 0, 0) );
-//        }
-//
-//        if( fj::Scalar kAX = (kRangeX.getMax() - kPosition.x()) < 0)
-//        {
-//            particle->addVelocity( m_K * kAX * fj::Vector3(-0.1, 0, 0) );
-//        }
-//
-//        if( fj::Scalar kY = (kRangeY.getMin() - kPosition.y()) > 0)
-//        {
-//            particle->addVelocity( m_K * kY * fj::Vector3(0, 1, 0) );
-//        }
-//
-//        if( fj::Scalar kY = (kRangeY.getMin() - kPosition.y()) < 0)
-//        {
-//            particle->addVelocity( m_K * kY * fj::Vector3(0, 1, 0) );
-//        }
-//
-//        if( fj::Scalar kZ = (kRangeZ.getMin() - kPosition.z()) > 0)
-//        {
-//            particle->addVelocity( m_K * kZ * fj::Vector3(0, 0, 1) );
-//        }
-//
-//        if( fj::Scalar kZ = (kRangeZ.getMin() - kPosition.z()) < 0)
-//        {
-//            particle->addVelocity( m_K * kZ * fj::Vector3(0, 0, 1) );
-//        }
-//
-//    }
+    auto iterator = particleSystem->getParticleManager().iterator();
+
+    while ( iterator->hasNext() )
+    {
+        const fj::Particle& kParticle = iterator->next();
+        const fj::ParticleID& kID = kParticle.getID();
+        const fj::Vector3& kPosition = kParticle.getPosition();
+        
+        if( fj::Scalar kIX = (kRangeX.getMin() - kPosition.x()) > 0)
+        {
+            particleSystem->addAccelAt(kID, m_K * kIX * fj::Vector3(1, 0, 0) );
+        }
+
+        if( fj::Scalar kAX = (kRangeX.getMax() - kPosition.x()) < 0)
+        {
+            particleSystem->addAccelAt(kID, m_K * kAX * fj::Vector3(-1, 0, 0) );
+        }
+
+        if( fj::Scalar kY = (kRangeY.getMin() - kPosition.y()) > 0)
+        {
+            particleSystem->addAccelAt(kID, m_K * kY * fj::Vector3(0, 1, 0) );
+        }
+
+        if( fj::Scalar kY = (kRangeY.getMax() - kPosition.y()) < 0)
+        {
+            particleSystem->addAccelAt(kID, m_K * kY * fj::Vector3(0, -1, 0) );
+        }
+
+        if( fj::Scalar kZ = (kRangeZ.getMin() - kPosition.z()) > 0)
+        {
+            particleSystem->addAccelAt(kID, m_K * kZ * fj::Vector3(0, 0, 1) );
+        }
+
+        if( fj::Scalar kZ = (kRangeZ.getMax() - kPosition.z()) < 0)
+        {
+            particleSystem->addAccelAt(kID, m_K * kZ * fj::Vector3(0, 0, -1) );
+        }
+
+    }
     
 }
