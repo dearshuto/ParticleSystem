e9582df1ba364c3b9d3ab8160d9988a00989f2e1
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-05-31 15:33:49 +0900

    [add] Add the new class for BB

---
+++ include/ParticleSystem/solver/bb_algorithm/partitioned_bounding_box.hpp
@@ -0,0 +1,14 @@
+//
+//  partitioned_bounding_box.hpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/05/31.
+//
+//
+
+#ifndef partitioned_bounding_box_hpp
+#define partitioned_bounding_box_hpp
+
+#include <stdio.h>
+
+#endif /* partitioned_bounding_box_hpp */
---
+++ src/solver/bb_algorithm/partitioned_bounding_box.cpp
@@ -0,0 +1,9 @@
+//
+//  partitioned_bounding_box.cpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/05/31.
+//
+//
+
+#include "partitioned_bounding_box.hpp"
