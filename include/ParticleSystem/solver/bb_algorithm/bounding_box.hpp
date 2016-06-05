be648de6af16cb99512a7ce4abe3b0b34e9eb490
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-06-05 14:55:27 +0900

    [refactor] Remove unused lines

--- include/ParticleSystem/solver/bb_algorithm/bounding_box.hpp
+++ include/ParticleSystem/solver/bb_algorithm/bounding_box.hpp
@@ -28,7 +28,7 @@ namespace fj {
     class BoundingBox;
 }
 
-class fj::BoundingBox// : public fj::BBAlgorithm
+class fj::BoundingBox
 {
 public:
    class Range
@@ -72,23 +72,7 @@ public:
     
 protected:
 
-
-    
-//    void update(const fj::ParticleManager& particleManager);
-    
     bool isWithinRange(const fj::Particle& particle)const;
-    
-//    bool isOutOfRange(const int x, const int y, const int z)const;
-//    
-//    void registerParticle(const fj::Particle& particle);
-//    
-//    void registerInBox(const fj::Particle& particle);
-    
-//    int convertPositionToIndex(const fj::Vector3& position)const;
-//    
-//    int convertPositionToIndex(const int x, const int y, const int z)const;
-//    
-//    int clamp(const int value, const Range& range)const;
     
 public:
 
@@ -111,8 +95,6 @@ public:
     {
         return m_inBox;
     }
-    
-//    const std::vector<fj::ParticleID>& get(const int x, const int y, const int z)const;
     
     const std::vector<fj::ParticleID>& getInBoxParticle()const
     {
--- src/solver/bb_algorithm/bounding_box.cpp
+++ src/solver/bb_algorithm/bounding_box.cpp
@@ -15,34 +15,6 @@
 
 #include <ParticleSystem/solver/bb_algorithm/bounding_box.hpp>
 
-//void fj::BoundingBox::update(const fj::ParticleManager &particleManager)
-//{
-//    auto iterator = particleManager.iterator();
-//    
-//    while (iterator->hasNext())
-//    {
-//        const fj::Particle& kParticle = iterator->next();
-//        
-//        registerParticle(kParticle);
-//    }
-//    
-//}
-
-//void fj::BoundingBox::registerParticle(const fj::Particle &particle)
-//{
-//    
-//    if ( isOutOfRange( particle ) )
-//    {
-//        m_outOfRangeParticle.push_back(particle.getID());
-//    }
-//    else
-//    {
-//        m_inBoxParticle.push_back(particle.getID());
-//    
-//        registerInBox( particle );
-//    }
-//    
-//}
 
 bool fj::BoundingBox::isWithinRange(const fj::Particle &particle)const
 {
@@ -66,53 +38,3 @@ bool fj::BoundingBox::isWithinRange(const fj::Particle &particle)const
     
     return true;
 }
-
-//bool fj::BoundingBox::isOutOfRange(const int x, const int y, const int z)const
-//{
-//    const int kIndex = convertPositionToIndex(x, y, z);
-//    
-//    if (m_inBox.size() <= kIndex)
-//    {
-//        return true;
-//    }
-//    
-//    return false;
-//}
-//
-//void fj::BoundingBox::registerInBox(const fj::Particle &particle)
-//{
-//    const fj::Vector3& kPosition = particle.getPosition();
-//    const int kIndex = convertPositionToIndex(kPosition);
-//    
-//    m_inBox[kIndex].push_back( particle.getID() );
-//}
-
-//int fj::BoundingBox::convertPositionToIndex(const fj::Vector3 &position)const
-//{
-//    const int kIndexX = (position.x() - getRangeX().getMin()) / getRangeX().getDivisionSize();
-//    const int kIndexY = (position.y() - getRangeY().getMin()) / getRangeY().getDivisionSize();
-//    const int kIndexZ = (position.z() - getRangeZ().getMin()) / getRangeZ().getDivisionSize();
-//    
-//    const int kClampedX = clamp(kIndexX, getRangeX());
-//    const int kClampedY = clamp(kIndexY, getRangeY());
-//    const int kClampedZ = clamp(kIndexZ, getRangeZ());
-//    
-//    return convertPositionToIndex(kClampedX, kClampedY, kClampedZ);;
-//}
-//
-//int fj::BoundingBox::convertPositionToIndex(const int x, const int y, const int z)const
-//{
-//    return x + getRangeX().getResolusion() * y + getRangeX().getResolusion() * getRangeY().getResolusion() * z;
-//}
-//
-//int fj::BoundingBox::clamp(const int value, const fj::BoundingBox::Range &range)const
-//{
-//    return std::max(0, std::min(value, range.getResolusion()) );
-//}
-//
-//const std::vector<fj::ParticleID>& fj::BoundingBox::get(const int x, const int y, const int z)const
-//{
-//    const int kIndex = convertPositionToIndex(x, y, z);
-//    
-//    return m_inBox[kIndex];
-//}
