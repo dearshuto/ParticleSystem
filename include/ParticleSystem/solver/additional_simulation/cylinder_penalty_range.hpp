f20ccd7e6fe4519fe880e05b71c7234fe13a964b
Shuto Shikama <shuto.shikama@fj.ics.keio.ac.jp>
2016-06-30 17:02:41 +0900

    [add] Make it support cylinder shape for penalty force

---
+++ include/ParticleSystem/solver/additional_simulation/cylinder_penalty_range.hpp
@@ -0,0 +1,14 @@
+//
+//  cylinder_penalty_range.hpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/06/30.
+//
+//
+
+#ifndef cylinder_penalty_range_hpp
+#define cylinder_penalty_range_hpp
+
+#include <stdio.h>
+
+#endif /* cylinder_penalty_range_hpp */
---
+++ src/solver/additional_simulation/cylinder_penalty_range.cpp
@@ -0,0 +1,9 @@
+//
+//  cylinder_penalty_range.cpp
+//  ParticleSystem
+//
+//  Created by Shuto on 2016/06/30.
+//
+//
+
+#include "cylinder_penalty_range.hpp"
